1109a7e06710b5dcfb21eccb8cffc4369ee4223e
Meghal <meghal.shah210@gmail.com>
2021-10-07 00:13:18 -0400

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,11 +17,13 @@ const createTweetElement = (tweet) => {
   const $hr = $('<hr>');
   
   const $createdAt = $('<p>').text(timeago.format(tweet['created_at']));
-  const $fontAwesome = $(`<span>
-  <i class="fa fa-flag iHover1" aria-hidden="true"></i>
-  <i class="fa fa-retweet iHover2" aria-hidden="true"></i>
-  <i class="fa fa-heart iHover3" aria-hidden="true"></i>
-  </span>`);
+
+  const $fontAwesome = $('<span>').addClass('font-awesome');
+
+  const $faFlag = $('<i>').addClass("fa fa-flag");
+  const $faRetweet = $('<i>').addClass("fa fa-retweet");
+  const $faHeart = $('<i>').addClass("fa fa-heart");
+  $fontAwesome.append($faFlag, $faRetweet, $faHeart);
   const $footer = $('<footer>').append($createdAt, $fontAwesome);
   
   const $tweet = $('<article>').addClass('tweet');
@@ -40,38 +42,21 @@ const renderTweets = (tweets) => {
   }
 };
 
+//make a get request to load tweets
+const loadtweets = () => {
+  $.get('/tweets/', (tweets) => {
+    renderTweets(tweets);
+  });
+}
+
+loadtweets();
+
+//When form is submitted, serialized the data and make a post request to create tweet and then load tweets
 $("#new-tweet-form").on("submit", (event) => {
     event.preventDefault();
     const serializedData = $("#new-tweet-form").serialize();
-
     $.post("/tweets/", serializedData, (response) => {
-      console.log(response);
-      renderTweets(data);
-    })
-  })
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
+    loadtweets();
+    });
+  });
 });
